135e65f8ff3be2e788d914e85996e89b4d8985e2
SakshamBeniwal05 <sakshambeniwal123@gmail.com>
2026-08-10 00:03:35 +0530

    updatye

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -61,20 +61,7 @@ export const projectsData: Project[] = [
 ];
 
 const Work: React.FC = () => {
-  const [activeFilter, setActiveFilter] = useState<'All' | 'Portraits' | 'Commercial' | 'Events'>('All');
-
-  const handleMouseMove = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    const btn = e.currentTarget;
-    const rect = btn.getBoundingClientRect();
-    const x = e.clientX - rect.left - rect.width / 2;
-    const y = e.clientY - rect.top - rect.height / 2;
-    btn.style.transform = `translate(${x * 0.25}px, ${y * 0.25}px)`;
-  };
-
-  const handleMouseLeave = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.currentTarget.style.transform = 'translate(0px, 0px)';
-  };
-
+  const [activeFilter, setActiveFilter] = useState<'All' | 'Portraits' | 'Commercial' | 'Events'>('All')
 
   const filteredProjects = activeFilter === 'All'
     ? projectsData
